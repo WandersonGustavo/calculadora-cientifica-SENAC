ea338b7d7b7849a3f5a2aa04992ac664d63f23fd
Wanderson Gustavo <62720366+WandersonGustavo@users.noreply.github.com>
2024-03-01 23:11:10 -0300

    #13

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,52 +1,49 @@
-import{soma} from "../javaScript/math/soma.js" 
-import{subtracao} from "../javaScript/math/subtracao.js" 
-import{multiplicacao} from "../javaScript/math/multiplicacao.js" 
+import { soma } from "./math/soma.js";
+import { subtracao } from "./math/subtracao.js";
+import { multiplicacao } from "./math/multiplicacao.js";
+import { divisao } from "./math/divisao.js";
+import { potencia } from "./math/potencia.js";
+import { raizQuadrada } from "./math/raiz-quadrada.js";
+import { media } from "./math/media.js";
+import { fatorial } from "./math/fatorial.js";
+import { porcentagem } from "./math/porcentagem.js";
 
-import{divisao} from "../javaScript/math/divisao.js" 
-import{raizQ} from "../javaScript/math/raizQ.js" 
-import{media} from "../javaScript/math/media.js" 
+export class App {
+    static initialize(document) {
 
-import{potencia} from "../javaScript/math/potencia.js" 
-import{fatorial} from "../javaScript/math/fatorial.js" 
-import{porcentagem} from "../javaScript/math/porcentagem.js" 
+        const btnCalcular = document.getElementById("btn-calcular");
+       
+        const num1 = document.getElementById("num1");
+        const num2 = document.getElementById("num2");
 
-export class App{
-    static initialize(document){
+        const resultadoSoma = document.getElementById("resultado-soma");
+        const resultadoSomaInstead = document.getElementById("resultado-soma-instead");
 
-        const btnCalcular= document.getElementById("btn-calcular");
-        const num1 =document.getElementById("num1");
-        const num2 =document.getElementById("num2");
+        const resultadoSubtracao = document.getElementById("resultado-subtracao");
+        const resultadoSubtracaoInstead = document.getElementById("resultado-subtracao-instead");
 
-        const resultadoSoma= document.getElementById("resultado-soma");
-        const resultadoSomaInstead= document.getElementById("resultado-soma-instead");
+        const resultadoMultiplicacao = document.getElementById("resultado-multiplicacao");
+        const resultadoMultiplicacaoInstead = document.getElementById("resultado-multiplicacao-instead");
 
-        const resultadoSubtracao= document.getElementById("resultado-subtracao");
-        const resultadoSubtracaoInstead= document.getElementById("resultado-subtracao-instead");
+        const resultadoDivisao = document.getElementById("resultado-divisao");
+        const resultadoDivisaoInstead = document.getElementById("resultado-divisao-instead");
 
-        const resultadoMultiplicacao= document.getElementById("resultado-multiplicacao");
-        const resultadoMultiplicacaoInstead= document.getElementById("resultado-multiplicacao-instead");
+        const resultadoRaizQuadrada = document.getElementById("resultado-raiz-quadrada");
+        const resultadoRaizQuadradaInstead = document.getElementById("resultado-raiz-quadrada-instead");
+        
+        const resultadoPotencia = document.getElementById("resultado-potencia");
+        const resultadoPotenciaInstead = document.getElementById("resultado-potencia-instead");
+        
+        const resultadoPorcentagem = document.getElementById("resultado-porcentagem");
+        const resultadoPorcentagemInstead = document.getElementById("resultado-porcentagem-instead");
 
-        const resultadoDivisao= document.getElementById("resultado-divisao");
-        const resultadoDivisaoInstead= document.getElementById("resultado-divisao-instead");
-
-        const resultadoPotencia= document.getElementById("resultado-potencia");
-        const resultadoPotenciaInstead= document.getElementById("resultado-potencia-instead");
-
-        const resultadoRaizQ= document.getElementById("resultado-raizQ");
-        const resultadoRaizQInstead= document.getElementById("resultado-raizQ-instead");
-
-        const resultadoMedia= document.getElementById("resultado-media");
-        const resultadoMediaInstead= document.getElementById("resultado-media-instead");
-
-        const resultadoPorcentagem= document.getElementById("resultado-porcentagem");
-        const resultadoPorcentagemInstead= document.getElementById("resultado-porcentagem-instead");
-
-        const resultadoFatorial= document.getElementById("resultado-fatorial");
-        const resultadoFatorialInstead= document.getElementById("resultado-fatorial-instead");
-
+        const resultadoFatorial = document.getElementById("resultado-fatorial");
+        const resultadoFatorialInstead = document.getElementById("resultado-fatorial-instead");
+        
+        const resultadoMedia = document.getElementById("resultado-media");
+        const resultadoMediaInstead = document.getElementById("resultado-media-instead");
 
         btnCalcular.addEventListener("click", () => {
-
             resultadoDivisao.innerHTML = divisao(num1.value, num2.value);
             resultadoDivisaoInstead.innerHTML = divisao(num2.value, num1.value);
 
@@ -59,24 +56,26 @@ export class App{
             resultadoMultiplicacao.innerHTML = multiplicacao(num1.value, num2.value);
             resultadoMultiplicacaoInstead.innerHTML = multiplicacao(num2.value, num1.value);
 
+            resultadoMultiplicacao.innerHTML = multiplicacao(num1.value, num2.value);
+            resultadoMultiplicacaoInstead.innerHTML = multiplicacao(num2.value, num1.value);
+
             resultadoPorcentagem.innerHTML = porcentagem(num1.value, num2.value) + "%";
             resultadoPorcentagemInstead.innerHTML = porcentagem(num2.value, num1.value) + "%";
 
             resultadoPotencia.innerHTML = potencia(num1.value, num2.value);
             resultadoPotenciaInstead.innerHTML = potencia(num2.value, num1.value);
 
-            resultadoRaizQ.innerHTML = raizQ(num1.value);
-            resultadoRaizQInstead.innerHTML = raizQ(num2.value);
+            resultadoRaizQuadrada.innerHTML = raizQuadrada(num1.value);
+            resultadoRaizQuadradaInstead.innerHTML = raizQuadrada(num2.value);
 
             resultadoSoma.innerHTML = soma(num1.value, num2.value);
             resultadoSomaInstead.innerHTML = soma(num2.value, num1.value);
 
             resultadoSubtracao.innerHTML = subtracao(num1.value, num2.value);
             resultadoSubtracaoInstead.innerHTML = subtracao(num2.value, num1.value);
-            
+
         });
-
-
+        
+        
     }
 }
-
---
+++ javaScript/math/multiplicacao.js
@@ -0,0 +1,4 @@
+
+export function multiplicacao (a, b) {
+    return Number(a) * Number(b);
+}
